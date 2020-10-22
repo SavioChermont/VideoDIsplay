049feb693868f56d5530399eb48a2b51b266b235
SavioChermont <saviochwt@gmail.com>
2020-10-22 14:06:39 -0300

    Cria estrutura de arquivos

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <SearchBar />
+            </div>
+         );
+    }
+}
+ 
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                SearchBar
+            </div>
+          );
+    }
+}
+ 
+export default SearchBar;
