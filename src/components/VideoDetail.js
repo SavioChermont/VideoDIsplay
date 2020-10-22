0b2a6203e8946424b4e63e9c3aa7b2c23d98c7f8
SavioChermont <saviochwt@gmail.com>
2020-10-22 17:12:49 -0300

    Adiciona ifram video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,9 +3,14 @@ import React from 'react';
 const VideoDetail = ({video}) => {
     if(!video)
         return <div>Loading...</div>
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     
     return ( 
             <div>
+                <div className="ui embed"> 
+                    <iframe src={videoSrc} title={video.snippet.title} />
+                </div>
                 <div className="ui segment">
                     <h4 className="ui header">{video.snippet.title}</h4>
                     <p>{video.snippet.description}</p>
@@ -15,3 +20,4 @@ const VideoDetail = ({video}) => {
 }
  
 export default VideoDetail;
+
